37523dcaadea1b869b77d7aea0751be4b1afba8b
Wumpie7 <chrisjaver@gmail.com>
2023-09-22 18:22:27 +0200

    Update DiscDownloader.py

--- Game Utilities/MC/YouTubeVideoToMusicDisc/DiscDownloader.py
+++ Game Utilities/MC/YouTubeVideoToMusicDisc/DiscDownloader.py
@@ -2,16 +2,32 @@ import pytube
 import moviepy.editor as mp
 import os
 
-VIDEO_URL = "https://youtu.be/qIzerUh9F-8"
+PLAYLIST_URL = "https://www.youtube.com/playlist?list=PLx7ZmhAvBjqizeG4FbI4htLZrwYPtVYgp"
 
-output_file = "disc.ogg"
+# Create a playlist object
+playlist = pytube.Playlist(PLAYLIST_URL)
 
-yt = pytube.YouTube(VIDEO_URL)
-stream = yt.streams.filter(only_audio=True).first()
-stream.download()
+# Directory to temporarily save downloaded videos
+download_dir = "./downloads"
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
 
-clip = mp.AudioFileClip(stream.default_filename)
-clip.write_audiofile(output_file)
-clip.close()
+# Iterate over all videos in the playlist
+for index, video_url in enumerate(playlist.video_urls):
+    yt = pytube.YouTube(video_url)
+    stream = yt.streams.filter(only_audio=True).first()
+    
+    # Download audio stream to the specified directory
+    downloaded_file = stream.download(output_path=download_dir)
+    
+    # Convert to ogg
+    output_file = os.path.join(download_dir, f"{yt.title}.ogg")
+    clip = mp.AudioFileClip(downloaded_file)
+    clip.write_audiofile(output_file)
+    clip.close()
+    
+    # Remove original downloaded file
+    os.remove(downloaded_file)
+    print(f"Processed {index + 1}/{len(playlist.video_urls)}: {yt.title}")
 
-os.remove(stream.default_filename)
+print("All videos processed!")
