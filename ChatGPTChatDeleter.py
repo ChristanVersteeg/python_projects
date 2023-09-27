5e5f0f4eefb9b502bd705568f6fca9fe6a7cfb04
Wumpie7 <christan.versteeg@hva.nl>
2023-09-27 15:03:08 +0200

    Added more functionality and rebindable keys

--- ChatGPTChatDeleter.py
+++ ChatGPTChatDeleter.py
@@ -6,31 +6,34 @@ import re
 paused = False
 chat_bin_coords = Vector2(230, 190)
 delete_prompt_coords = Vector2(1125, 630)
+delete = 'delete'       #Removes first chat that is not selected. Both delete and backspace are used for bulk removal,   
+backspace = 'backspace' #Removes first chat that is selected.     if the first chat is selected backspace should be used else delete.
+esc = 'esc'             #Removes chat that the cursor selects on top of the bin.
+f2 = 'f2'               #Pauses the script.
+home = 'home'           #Finds coordinates of your cursor, used to configure this script.
 
 def print_mouse_position(event):
-    if event.name == 'home': 
+    if event.name == home: 
         print(', '.join(re.findall(r'\d+', str(auto.position()))))
 key.on_press(print_mouse_position)
 
 def print_mouse_position(event):
-    if event.name == 'end': 
+    if event.name == f2: 
         global paused
         paused = not paused
 key. on_press(print_mouse_position)
-
-
 
 def delete(event):
     def matches_any_key(*keys):
         return event.name in keys
        
     if(paused): return
-    if not matches_any_key('delete', 'backspace', 'page up'): return
+    if not matches_any_key(delete, backspace, esc): return
 
-    if event.name == 'delete': #Removes first chat that is not selected
+    if event.name == delete: 
         auto.click(chat_bin_coords.x, chat_bin_coords.y)
-    #And backspace removes first chat that is selected    
-    if event.name != 'page up': #Removes chat that the cursor selects on top of the bin
+
+    if event.name != esc: 
         auto.click(chat_bin_coords.x, chat_bin_coords.y)
     else: 
         auto.click()
@@ -38,7 +41,7 @@ def delete(event):
     
     auto.click(delete_prompt_coords.x, delete_prompt_coords.y)
         
-    if(event.name == 'page up'):
+    if(event.name == esc):
         auto.moveTo(x, y)
 key.on_press(delete)
 
