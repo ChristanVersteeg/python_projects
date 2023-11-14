8bef2c9da2144b3f6e5edbad3f86edbaebd6e469
Wumpie7 <chrisjaver@gmail.com>
2023-11-14 23:38:53 +0100

    how did this work first try ChatGPT is actually cracked

--- godthisisstupidbutelseillgetconfused.py
+++ godthisisstupidbutelseillgetconfused.py
@@ -2,13 +2,18 @@ import tkinter as tk
 import keyboard as key
 
 warn_window = None
+last_position = None  # Variable to store the last position
 
-def make_label_window(text):
+def make_label_window(text, position=None):
     window = tk.Toplevel()
     window.overrideredirect(True)
     window.attributes('-topmost', True)
     label = tk.Label(window, text=text, bg="lightgrey", borderwidth=2, relief="solid")
     label.pack()
+
+    # Set initial position if provided
+    if position:
+        window.geometry(f"+{position[0]}+{position[1]}")
 
     def start_move(event):
         window.x = event.x
@@ -32,12 +37,13 @@ def make_label_window(text):
     return window
 
 def toggle_window():
-    global warn_window
+    global warn_window, last_position
     if warn_window is None:
-        # Create the window
-        warn_window = make_label_window("PARROT DO NOT FORGET TO DISABLE/ENABLE YOUR PACE PINGS YOU UTTER BUFFOON")
+        # Create the window with the last saved position
+        warn_window = make_label_window("PARROT DO NOT FORGET TO DISABLE/ENABLE YOUR PACE PINGS YOU UTTER BUFFOON", last_position)
     else:
-        # Destroy the window
+        # Save the current position before destroying
+        last_position = (warn_window.winfo_x(), warn_window.winfo_y())
         warn_window.destroy()
         warn_window = None
 
