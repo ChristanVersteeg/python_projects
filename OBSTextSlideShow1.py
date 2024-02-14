8dd0ad527e3bf205d76bd7b859033ed14a8dd9a4
Wumpie7 <chrisjaver@gmail.com>
2024-02-14 21:32:49 +0100

    Space continue is now synced with key continue

--- OBSTextSlideShow1.py
+++ OBSTextSlideShow1.py
@@ -14,17 +14,23 @@ display_texts = [
     "-Why do you want to work in Algorithmics?",
     "-Why should we hire you?"
     ]
+display_texts_length = len(display_texts)
 
 def display_next_text(event):
     global index
     
-    with open(text_file, 'w') as f: f.write(display_texts[index])
-    
     index += 1
+    
+    with open(text_file, 'w') as f: f.write(display_texts[index % display_texts_length])
+    
 keyboard.on_press_key(trigger_key, display_next_text, suppress=True)
 
 def display_text_by_key(event):
-    with open(text_file, 'w') as f: f.write(display_texts[int(event.name)])
+    global index
+    
+    index = int(event.name)
+    
+    with open(text_file, 'w') as f: f.write(display_texts[index])
     
 for i in range(10):
     keyboard.on_press_key(str(i), display_text_by_key, suppress=True)
