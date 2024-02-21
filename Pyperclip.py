27a8762b82de3119f396c7020e79e122a3026a2d
Wumpie7 <chrisjaver@gmail.com>
2024-02-22 00:27:34 +0100

    Coppier6000

--- Pyperclip.py
+++ Pyperclip.py
@@ -1,7 +1,6 @@
 import pyperclip
 
-user_input = input("Type the word you want to gluckify: ")
-
-print(f"Gluckified \"{user_input}\". It has been saved to the clipboard, you can paste it anywhere with CTRL+V.")
-
-pyperclip.copy(f"gl{user_input}")
+while(True):
+    
+    pyperclip.waitForNewPaste()
+    with open("transcript.txt", 'a') as f: f.write(f"{pyperclip.paste()}\n")
