5d1b60ceabcd50e7ae3bc4069f615d3fe719fc0f
Wumpie7 <chrisjaver@gmail.com>
2024-04-09 01:28:13 +0200

    Made the OCR more reliable by post processing
    Also improved the ValueError feedback to the user.

--- ImageToRelativeTime.py
+++ ImageToRelativeTime.py
@@ -1,16 +1,15 @@
 from datetime import datetime, timedelta
 from io import BytesIO
-from PIL import Image
-from PIL import ImageGrab
+from PIL import Image, ImageGrab, ImageEnhance, ImageFilter
 from pyperclip import copy
 from pytesseract import image_to_string
-from re import match
+from re import match, IGNORECASE
 
 def parse_to_minutes(time):
     total_minutes = None
     
     def check_format(regex, mult_x, mult_y):
-        matched_format = match(regex, time)
+        matched_format = match(regex, time, IGNORECASE)
         
         if not matched_format: return
         
@@ -21,18 +20,26 @@ def parse_to_minutes(time):
     check_format(r"(\d+)D (\d+)H", 1440, 60)
     check_format(r"(\d+)H (\d+)M", 60, 1)
     
-    if total_minutes is None: raise ValueError("Time format is not recognized.")
+    if total_minutes is None: 
+        raise ValueError(f"Format is not recognized, given format: {time if time != '' else 'FORMAT YIELDED EMPTY STRING.'}Make sure the format is either (0D/d 0H/h) or (0H/h 0M/m) (0 can be any number, and no brackets).")
     else: return total_minutes
 
 def image_to_timestamp():
     image = ImageGrab.grabclipboard()
-    byte_array = BytesIO()
     
+    if image is None: raise ValueError("Clipboard did not have an image copied.")
+    
+    byte_array = BytesIO()
     image.save(byte_array, format='PNG')
     byte_array.seek(0)
     
     image = Image.open(byte_array)
-    time = image_to_string(image, lang="eng")
+    image_enhance = ImageEnhance.Contrast(image).enhance(2)
+    image_filter = image_enhance.filter(ImageFilter.SHARPEN)
+    image_gray_scale = image_filter.convert('L')
+    image_black_white = image_gray_scale.point(lambda x: 0 if x < 128 else 255, '1')
+    
+    time = image_to_string(image_black_white, lang="eng", config='--psm 6')
     time = time.replace("O", "0")
     
     current_time = datetime.now()
