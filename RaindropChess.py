943f8dd86aa90f84d5ed29145d1696ba004efbf2
Wumpie7 <christan.versteeg@hva.nl>
2024-07-24 12:46:58 +0200

    Pieces get displayed with Tkinter

--- RaindropChess.py
+++ RaindropChess.py
@@ -1,5 +1,6 @@
-from PIL import Image
+from PIL import Image, ImageTk
 from enum import Enum
+import tkinter as tk
 
 class Piece(Enum):
     KING = 0
@@ -13,8 +14,8 @@ class Color(Enum):
     WHITE = 0
     BLACK = 1
 
-def split_image(image_path):
-    image = Image.open(image_path)
+def split_pieces(image_path):
+    image = Image.open(image_path).convert("RGBA")
     sub_image_width = 132
     sub_image_height = 132
     columns = 6
@@ -38,5 +39,31 @@ def split_image(image_path):
             pieces[(piece_name, color)] = sub_image
     
     return pieces
-pieces = split_image("C:/Users/pooti/Desktop/python_projects/ChessPieces.png") 
-print(pieces)
+
+def display_pieces_tkinter(pieces):
+    keys = list(pieces.keys())
+    index = 0
+    
+    root = tk.Tk()
+    label = tk.Label(root)
+    label.pack()
+
+    def show_image(idx):
+        image = pieces[keys[idx]]
+        photo = ImageTk.PhotoImage(image)
+        label.config(image=photo)
+        label.image = photo
+        root.title(f"{keys[idx][1].name.capitalize()} {keys[idx][0].name.capitalize()}")
+
+    def on_space(event):
+        nonlocal index
+        index = (index + 1) % len(keys)
+        show_image(index)
+
+    root.bind("<space>", on_space)
+    show_image(index)
+    root.mainloop()
+
+pieces = split_pieces("C:/Users/pooti/Desktop/python_projects/ChessPieces.png")
+
+display_pieces_tkinter(pieces)
