b8fe31a8db3dfe8b69f73e09d61233a4c2baa437
Wumpie7 <christan.versteeg@hva.nl>
2024-07-24 13:25:01 +0200

    Finished the main function of the script
    Only tweaks will be made

--- RaindropChess.py
+++ RaindropChess.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageTk
 from enum import Enum
 import tkinter as tk
+import random
 
 class Piece(Enum):
     KING = 0
@@ -40,30 +41,43 @@ def split_pieces(image_path):
     
     return pieces
 
-def display_pieces_tkinter(pieces):
-    keys = list(pieces.keys())
-    index = 0
+def create_random_stack():
+    pieces = (
+        [Piece.QUEEN] +
+        [Piece.ROOK] * 2 +
+        [Piece.BISHOP] * 2 +
+        [Piece.KNIGHT] * 2 +
+        [Piece.PAWN] * 8
+    )
     
+    random.shuffle(pieces)
+    
+    return pieces
+
+def display_pieces_tkinter(pieces, stacks):
     root = tk.Tk()
     label = tk.Label(root)
     label.pack()
 
-    def show_image(idx):
-        image = pieces[keys[idx]]
-        photo = ImageTk.PhotoImage(image)
-        label.config(image=photo)
-        label.image = photo
-        root.title(f"{keys[idx][1].name.capitalize()} {keys[idx][0].name.capitalize()}")
+    def show_piece(color):
+        stack = stacks[color.value]
+        if stack:
+            piece = stack.pop()
+            
+            print(f"{color} Cards remaining: {len(stack)}")
+            
+            image = pieces[(piece, color)]
+            photo = ImageTk.PhotoImage(image)
+            label.config(image=photo)
+            label.image = photo
 
-    def on_space(event):
-        nonlocal index
-        index = (index + 1) % len(keys)
-        show_image(index)
+    root.bind("<Button-1>", lambda event: show_piece(Color.WHITE))
+    root.bind("<Button-3>", lambda event: show_piece(Color.BLACK))
 
-    root.bind("<space>", on_space)
-    show_image(index)
     root.mainloop()
 
 pieces = split_pieces("C:/Users/pooti/Desktop/python_projects/ChessPieces.png")
 
-display_pieces_tkinter(pieces)
+stacks = [create_random_stack(), create_random_stack()]
+
+display_pieces_tkinter(pieces, stacks)
